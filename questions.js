6e6d2816a7067e1d18cab4418ff6cc140c0f4f01
preethipattabiraman <44402499+preethipattabiraman@users.noreply.github.com>
2021-03-26 14:35:15 +0530

    Initial Commit
    Quiz CLI

---
+++ questions.js
@@ -0,0 +1,95 @@
+var Questions = [
+    {
+      question : `What is Terry's favourite food?
+      a. Milk
+      b. Yogurt
+      c. Protein powder
+      \n`,
+      answer : "b"
+  },
+  
+  {
+    question : `Which precinct of the NYPD is Brooklyn 99 set in?
+    a. 11
+    b. 99
+    c. 22
+    \n`,
+    answer : "b"
+  },
+  
+  {
+    question : `Which character does Joe Lo Truglio play?
+    a. Pimento
+    b. Jake Peralta
+    c. Charles Boyle
+    \n`,
+    answer : "c"
+  },
+  
+  {
+    question : `Who plays violin at Jake and Amy's wedding?
+    a. Mleponos
+    b. Boyle
+    c. Scully
+    \n`,
+    answer : "a"
+  },
+  
+  {
+    question : `What is Nutriboom?
+    a. New drug
+    b. Nickname for Terry
+    c. A pyramid scheme
+    \n`,
+    answer : "c"
+  },
+  
+  {
+    question : `Which two character's parents get married?
+    a. Boyle's and Linetti's
+    b. Santiago's and Diaz's
+    c. Boyle's and Diaz's
+    \n`,
+    answer : "a"
+  },
+  
+  {
+    question : `What does Jake attempt to steal from Holt in the first Halloween episode?
+    a. Cummerband
+    b. Watch
+    c. Shoe
+    \n`, 
+    answer : "b"
+  },
+  
+  {
+    question : `What is the Vulture's real name?
+    a. Pembroke
+    b. Judy
+    c. Pimento
+    \n`,
+    answer : "a"
+  },
+  
+  {
+    question : `What drug does the team try to take down in season two?
+    a. SillyPowder
+    b. GigglePig
+    c. White
+    \n`,
+    answer : "b"
+  },
+  
+  {
+    question : `What did Holt do with Wuntch when they met Mitchelle Obama?
+    a. Pushed Wuntch into Mitchelle Omaba
+    b. Made a dig of Wuntch
+    c. Spoke nonsense about Wuntch
+    \n`,
+    answer : "a"
+  }
+  
+  ];
+  
+  module.exports = Questions;
+  
